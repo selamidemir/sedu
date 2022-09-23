0fda4e03e67999482acd67f563daa88cea6341da
Selami Demir <selamidemir@gmail.com>
2022-09-23 09:21:01 +0300

    View bölümü hazırlandı

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('ANA SAYFA'));
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.status(200).render('index', { pageName: 'index'}));
+app.get('/about', (req, res) => res.status(200).render('about', { pageName: 'about'}));
 
 app.listen(port, () =>
   console.log(`Suncucu ${port} nolu porttan ayağa kalktı`)
