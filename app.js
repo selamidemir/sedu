32d4dc478418f2f9c9f4779a7930c1c994dad13f
Selami Demir <selamidemir@gmail.com>
2022-09-23 10:50:57 +0300

    Route bölümü eklendi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const pageRoute = require('./routes/pageRoutes')
 
 dotenv.config();
 
@@ -13,8 +14,8 @@ app.set('view engine', 'ejs');
 // Middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.status(200).render('index', { pageName: 'index'}));
-app.get('/about', (req, res) => res.status(200).render('about', { pageName: 'about'}));
+// Routes
+app.use('/', pageRoute);
 
 app.listen(port, () =>
   console.log(`Suncucu ${port} nolu porttan ayağa kalktı`)
---
+++ controlles/pageControllers.js
@@ -0,0 +1,4 @@
+exports.getIndexPage = (req, res) => res.status(200).render('index', { pageName: 'index'});
+
+exports.getAboutPage = (req, res) => res.status(200).render('about', { pageName: 'about'});
+
