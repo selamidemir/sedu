10c6e411f9dda6e4345d9be778d4e6d2560f6bca
Selami Demir <selamidemir@gmail.com>
2022-10-05 19:06:18 +0300

    İletişim formu eklendi

--- controlles/pageControllers.js
+++ controlles/pageControllers.js
@@ -1,4 +1,45 @@
-exports.getIndexPage = (req, res) => res.status(200).render('index', { pageName: 'index'});
+const nodemailler = require('nodemailer');
 
-exports.getAboutPage = (req, res) => res.status(200).render('about', { pageName: 'about'});
+exports.getIndexPage = (req, res) =>
+  res.status(200).render('index', { pageName: 'index' });
 
+exports.getAboutPage = (req, res) =>
+  res.status(200).render('about', { pageName: 'about' });
+
+exports.getContactPage = (req, res) =>
+  res.status(200).render('contact', { pageName: 'contact' });
+
+exports.sendMail = async (req, res) => {
+  const message = `
+        <h1>Email Details</h1>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>EMail: ${req.body.email}</li>
+        </ul>
+        <h2>Message</h2>
+        <p>${req.body.message}</p>
+    `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = await nodemailler.createTransport({
+    host: 'smtp.google.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.APP_EMAIL_USER, // generated ethereal user
+      pass: process.env.APP_EMAIL_PASS, // generated ethereal password
+    },
+  });
+  console.log("bölüm 2")
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"Sedu Contact Mail" <${process.env.APP_EMAIL_USER}>`, // sender address
+    to: `${process.env.APP_EMAIL_TO}, ${process.env.APP_EMAIL_TO}`, // list of receivers
+    subject: 'Sedu Contact Mail New Message ✔', // Subject line
+    html: message,
+  });
+
+  console.log('Message sent: %s', info.messageId);
+
+  res.redirect('/contact');
+};
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
+router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendMail);
 
 module.exports = router;
